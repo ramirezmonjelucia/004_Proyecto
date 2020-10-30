e10b3067e7c8bf4cdf0d1501eee79af61218f6c5
Lucia Ramirez Monje <lramirez@iespuntadelverde.es>
2020-10-30 19:59:20 +0100

    first commit

--- src/ejercicio001.js
+++ src/ejercicio001.js
@@ -1,22 +1,41 @@
 /* 
 Insertar datos
 */
-db.inventory.insertMany([
-    { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
-    { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
-    { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
-    { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
-    { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
- ]);
+db.inventory.insertOne({item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" });
+db.inventory.insertOne({item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" });
+db.inventory.insertOne({item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "pencil", qty: 20, size: { h: 4, w: 29.47, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "carpet", qty: 150, size: { h: 60, w: 44, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "mechanical pencil", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "mines", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "brushes", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "canvas", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "racks", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "easels", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "plasticine", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "washi tapes", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "watercolors", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "varnish", qty: 20, size: { h: 4, w: 15.50, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "canvas", qty: 100, size: { h: 28, w: 35.5, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "mat", qty: 85, size: { h: 27.9, w: 35.5, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "mousepad", qty: 25, size: { h: 19, w: 22.85, uom: "cm" }, status: "P" });
+db.inventory.insertOne({item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "P" });
+db.inventory.insertOne({item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" });
+db.inventory.insertOne({item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" });
+db.inventory.insertOne({item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "sketchbook", qty: 80, size: { h: 14, w: 21, uom: "cm" }, status: "A" });
+db.inventory.insertOne({item: "sketch pad", qty: 95, size: { h: 22.85, w: 30.5, uom: "cm" }, status: "A" });
  /*
  CAMBIAR LA INFORMACÍON INICIAL
  */
- db.inventory.insertOne(
-    { item: "ebook", qty: 10, size: { h: 8, w: 10, uom: "cm" }, status: "C" }
+ db.inventory.insertOne({ item: "ebook", qty: 10, size: { h: 8, w: 10, uom: "cm" }, status: "C" }
 );
  
 /*
 BORRAR INFORMACIÓN
 */
-db.inventory.drop({ item: "ebook", qty: 10, size: { h: 8, w: 10, uom: "cm" }, status: "A" }
+db.inventory.drop({item: "ebook", qty: 10, size: { h: 8, w: 10, uom: "cm" }, status: "A" }
 );
--- src/pruebafind.js
+++ src/pruebafind.js
@@ -3,28 +3,36 @@ BÚSQUEDAS FIND PARA ENONTRAR INFORMACIÓN
 */
 db.inventory.find({status: "D"})
 
-db.inventory.find({status: { $eq: "D" } })
+db.inventory.find({status: {$eq: "D" }})
 
-db.inventory.find( { status: { $in: [ "A", "D" ] } } )
+db.inventory.find({status: {$in: ["A", "D"]}})
 
-db.inventory.find( { $or: [ { status: {$eq: "A" } }, { status: {$eq: "D"} } ] } )
-
-db.inventory.find( {
-    status: "A",
-    $or: [ { qty: { $lt: 30 } }, { item: /^p/ } ]
-} );
+db.inventory.find({$or: [{status: {$eq: "A"}}, {status: {$eq: "D"}}]});
 /*
 Número de documentos que cumple la condición
+Poniedo .count al final en vez de una palabra nos define una cantidad
 */
 db.inventory.find( { status: { $in: [ "A", "D" ] } } ).count()
 
 db.inventory.find({}).count()
 
-db.inventory.find( { $or: [ { status: {$eq: "A" } }, { status: {$eq: "D"} } ] } ).count()
+db.inventory.find({$or: [{status: {$eq: "A" }}, {status: {$eq: "D"}}]}).count()
 
 /*
 No se puede usar el mismo campo en un and implicito
 */
-db.inventory.find({
-    $and: [{qty: {$lt: 55}}, {qty: {$gt: 45}}]
-})
+db.inventory.find({$and: [{qty: {$lt: 55}}, {qty: {$gt: 45}}]});
+/*
+Busca el estatus A ó que qly sea menor que 30
+*/
+db.inventory.find({status: "A",$or: [{qty: {$lt: 30}}]})              
+
+/*
+Nos devuelve un qty que exista y sea 100
+*/
+db.inventory.find({qty: {$exists: true, $gt: 50}})
+
+/*
+Nos devuelve un qty que no sea 100
+*/
+db.inventory.find({qty: {$ne: 100}}).pretty()
